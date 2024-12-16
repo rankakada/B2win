fa1a63f60bcd36eb62a5f66fddcf3b24dae12a4e
Ran Kakada <rankakada10@gmail.com>
2024-12-16 11:11:22 +0700

    update promotion modal

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+const promotionModal = document.querySelector(".promotion-modal");
+const btnOpenModal = document.querySelectorAll(".open-modal");
+const btnCloseModal = document.querySelectorAll(".close-modal");
+
+const toggleModal = (isVisible) => {
+    promotionModal.style.display = isVisible ? "block" : "none";
+}
+
+btnOpenModal.forEach(btn => btn.addEventListener("click", () => toggleModal(true)));
+btnCloseModal.forEach(btn => btn.addEventListener("click", () => toggleModal(false)));
+
+promotionModal.addEventListener("click", (e)=>{
+    if(e.target === promotionModal) {
+        promotionModal.style.display = "none";
+    }
+});
