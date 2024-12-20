4be6a7f812b553c0a5e4a74cae5637961152d047
Ran Kakada <rankakada10@gmail.com>
2024-12-20 14:36:57 +0700

    validate registerForm

--- static/js/register.js
+++ static/js/register.js
@@ -37,6 +37,12 @@ username.addEventListener("input", () => {
     username.value = username.value.toUpperCase();
 });
 
+// affiliate input to uppercase
+
+affiliate.addEventListener("input", () => {
+    affiliate.value = affiliate.value.toUpperCase();
+});
+
 // From validation
 
 registerForm.onsubmit = (e) => {
@@ -49,27 +55,110 @@ const validateInputs = () => {
     let passwordValue = password.value.trim();
     let numberPhoneValue = numberPhone.value.trim();
     let affiliateValue = affiliate.value.trim();
-    let codeValue = code.value.trim();
+    let codeValue = randomCode.value.trim();
+    let randomCodeValue = code.value.trim();
+    let userErrorMsg = document.getElementById("userErrorMsg");
+    let passErrorMsg = document.getElementById("passErrorMsg");
+    let phoneErrorMsg = document.getElementById("phoneErrorMsg");
+    const affiliateStore = ["B2000000", "B2000001", "B2000002", "B2000003", "B2000004", "B2000005"];
+
     const textRegex = /^[A-Z0-9]+$/;
     const numberRegex = /^\d/;
+    const cambodianPhoneRegex = /^(?:\+855[1-9]\d{7,8}|0[1-9]\d{7,8})$/;
+
+    // function to add and clear error messange
+
+    const setError = (element, errorMsg, errorContainer) => {
+        element.classList.add("error");
+        errorContainer.innerHTML = errorMsg;
+    }
+
+    const clearError = (element, errorContainer) => {
+        element.classList.remove("error");
+        errorContainer.innerHTML = "";
+    }
+
+    let isValid = true; //Track overall validation
+
+    // Check username input
+
+    if(usernameValue === "") {
+        setError(username, "សូមបំពេញឈ្មោះអ្នកប្រើប្រាស់", userErrorMsg);
+            isValid = false;
+    }
+    else if(usernameValue.length < 6 ) {
+        setError(username, "ឈ្មោះអ្នកប្រើប្រាស់ត្រូវមានតួអក្សរយ៉ាងតិច6តួ", userErrorMsg);
+        isValid = false;
+    }
+    else if(usernameValue.length > 16 ) {
+        setError(username, "ឈ្មោះអ្នកប្រើប្រាស់ត្រូវមានតួអក្សរច្រើនបំផុត16តួ", userErrorMsg);
+        isValid = false;
+    }
+    else if (!textRegex.test(usernameValue)) { 
+        setError(username, "ឈ្មោះអ្នកប្រើប្រាស់ត្រូវមានតួអក្សរ និងលេខប៉ុណ្ណោះ", userErrorMsg);
+        isValid = false;
+    }
+    else if (numberRegex.test(usernameValue)) { 
+        setError(username, "ឈ្មោះអ្នកប្រើប្រាស់មិនអាចចាប់ផ្តើមដោយលេខ", userErrorMsg);
+        isValid = false;
+    }
+    else {
+        clearError(username, userErrorMsg);
+    }
+
+    // Check password input
+
+    if(passwordValue === ""){
+        setError(password, "សូមបំពេញលេខសម្ងាត់", passErrorMsg);
+        isValid = false;
+    }
+    else if(passwordValue.length < 6) {
+        setError(password, "ពាក្យសម្ងាត់ត្រូវមានយ៉ាងតិច6តួ", passErrorMsg);
+        isValid = false;
+    }
+    else if(passwordValue.length > 16) {
+        setError(password, "ពាក្យសម្ងាត់ត្រូវមានច្រើនបំផុត16តួ", passErrorMsg);
+        isValid = false;
+    }
+    else {
+       clearError(password, passErrorMsg);
+    }
+
+    // Check phone number input
+
+    if(numberPhoneValue === "") {
+        setError(numberPhone, "សូមបំពេញលេខទូរសព្ទ", phoneErrorMsg);
+        isValid = false;
+    }
+    else if (!numberPhoneValue.startsWith("+855") && !numberPhoneValue.startsWith("0")) {
+        setError(numberPhone, "លេខទូរសព្ទត្រូវចាប់ផ្ដើមដោយ +855 ឬ លេខ 0", phoneErrorMsg);
+    }
+    else if(!cambodianPhoneRegex.test(numberPhoneValue)) {
+        setError(numberPhone, "លេខទូរសព្ទមិនត្រឹមត្រូវ​ សូមពិនិត្យម្ដងទៀត", phoneErrorMsg);
+        isValid = false;
+    }
+    else {
+        clearError(numberPhone, phoneErrorMsg);
+    }
+
+    //  Register status
+
+    if(isValid) {
+        if(affiliateValue !== "" && !affiliateStore.includes(affiliateValue)) {
+            registerStatus.innerHTML = "ឈ្មោះអ្នកណែនាំមិនត្រឹមត្រូវ";
+            registerStatus.className = "registerError";
+        }
+        else if (codeValue === "" || codeValue!== randomCodeValue) {
+            registerStatus.innerHTML = "លេខកូដសម្ងាត់មិនត្រឹមត្រូវ";
+            registerStatus.className = "registerError";
+        }
+        else {
+           registerStatus.innerHTML = "ការបង្កើតគណនីទទួលបានជោគជ័យ";
+           registerStatus.className = "registerSuccess";
+           registerForm.reset();
+        }
+    }
+    else {
+        registerStatus.innerHTML = ""; 
+    }
 }
-
-// function to add and clear error messange
-
-const setError = (element, errorMsg, errorContainer) => {
-    element.classList.add("error");
-    errorContainer.innerHTML = errorMsg;
-}
-
-const clearError = (element, errorContainer) => {
-    element.classList.remove("error");
-    errorContainer.innerHTML = errorMsg;
-}
-
-let isValid = true; //Track overall validation
-
-// Check username input
-
-// if(usernameValue === "") {
-
-// }
