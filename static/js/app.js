6b8f6e874415475b7bc878065ea8bb03d375b050
Ran Kakada <rankakada10@gmail.com>
2024-12-21 16:26:58 +0700

    respone home

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,22 @@
+// Menu Modal
+
+const mobileMenu = document.querySelector(".mobile-menu");
+const btnOpenMenu = document.querySelector(".open-menu");
+const btnCloseMenu = document.querySelector(".btn-close-menu");
+
+const toggleMenu = (isVisible) => {
+    mobileMenu.style.display = isVisible ? "block" : "none";
+}
+
+btnOpenMenu.addEventListener("click", () => toggleMenu(true));
+btnCloseMenu.addEventListener("click", () => toggleMenu(false));
+
+mobileMenu.addEventListener("click", (e) => {
+    if(e.target === mobileMenu) {
+        mobileMenu.style.display = "none";
+    }
+});
+
 // Promotion Modal homepage
 
 const promotionModal = document.querySelector(".promotion-modal");
